e6c46886eeaa1b9371fd7be68e07462336f8a132
Furkan Sabaz <furkan.sabaz@gmail.com>
2019-07-31 15:23:48 +0300

    Initial Commit

---
+++ TinderClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderClone
+//
+//  Created by Furkan Sabaz on 31.07.2019.
+//  Copyright © 2019 Furkan Sabaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
