22db2c6cddc217c575ee9a3707b7311aefa481fb
Furkan Sabaz <furkan.sabaz@gmail.com>
2019-08-26 13:15:49 +0300

    Ders 34-Kullanıcı Detaylarına Profil Fotoğrafı Eklemek

--- TinderClone/Controller/Kullanici Detaylari/KullaniciDetaylariController.swift
+++ TinderClone/Controller/Kullanici Detaylari/KullaniciDetaylariController.swift
@@ -10,12 +10,73 @@ import UIKit
 
 class KullaniciDetaylariController: UIViewController {
 
+    
+    
+    lazy var scrollView : UIScrollView = {
+        let sv = UIScrollView()
+        sv.alwaysBounceVertical = true
+        sv.contentInsetAdjustmentBehavior = .never
+        sv.delegate = self
+        return sv
+        
+    }()
+    
+    
+    let imgProfil : UIImageView = {
+        
+        let img = UIImageView(image: #imageLiteral(resourceName: "apple"))
+        img.contentMode = .scaleAspectFill
+        img.clipsToBounds = true
+        return img
+    }()
+    
+    let lblBilgi : UILabel = {
+        let lbl = UILabel()
+        
+        lbl.text = "Sibel Kara 40\nÖğretmen\nÖğrencilerimi çok severim"
+        lbl.numberOfLines = 0
+        return lbl
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        
+        
+        view.addSubview(scrollView)
+        scrollView.doldurSuperView()
+        
+        
+        scrollView.addSubview(imgProfil)
+        imgProfil.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.width)
+        
+        
+        scrollView.addSubview(lblBilgi)
+        _ = lblBilgi.anchor(top: imgProfil.bottomAnchor, bottom: nil, leading: scrollView.leadingAnchor, trailing: scrollView.trailingAnchor, padding: .init(top: 16, left: 16, bottom: 0, right: 16))
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapGestureKapat)))
+        
+    }
+    
+    @objc fileprivate func tapGestureKapat() {
+        self.dismiss(animated: true)
     }
     
 
 
+}
+
+extension KullaniciDetaylariController : UIScrollViewDelegate{
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let yFark = scrollView.contentOffset.y
+        print("Y Fark : ",yFark)
+        var width = view.frame.width - 2*yFark
+        width = max(view.frame.width,width)
+        
+        
+        imgProfil.frame = CGRect(x: min(0,yFark), y: min(0,yFark), width: width, height: width)
+        
+    }
 }
