ea58870c36777053155f10cc124631619210a562
Furkan Sabaz <furkan.sabaz@gmail.com>
2019-09-26 08:33:08 +0300

    Ders 55-Mesajların Görüntüleneceği Bölümü Oluşturma

--- TinderClone/Controller/EslesmelerMesajlar/MesajKayitController.swift
+++ TinderClone/Controller/EslesmelerMesajlar/MesajKayitController.swift
@@ -11,13 +11,62 @@ import UIKit
 
 struct Mesaj {
     let text : String
+    let benimMesajim : Bool
 }
 
 class MesajCell : ListeCell<Mesaj> {
+    
+    let mesajContainer = UIView(arkaPlanRenk: #colorLiteral(red: 0.881370306, green: 0.8761312366, blue: 0.885397613, alpha: 1))
+    // gönderilen veya alınan mesajı tutar - gösterir
+    let txtMesaj : UITextView  = {
+        
+        let txt = UITextView()
+        txt.backgroundColor = .clear
+        txt.font = .systemFont(ofSize: 20)
+        txt.isScrollEnabled = false
+        txt.isEditable = false
+        return txt
+    }()
+    
     override var veri: Mesaj! {
         didSet {
-            backgroundColor = .blue
+            
+            txtMesaj.text = veri.text
+            
+            if veri.benimMesajim {
+                mesajConstraint.trailing?.isActive = true
+                mesajConstraint.leading?.isActive = false
+                mesajContainer.backgroundColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
+                txtMesaj.textColor = .white
+            } else {
+                mesajConstraint.trailing?.isActive = false
+                mesajConstraint.leading?.isActive = true
+                mesajContainer.backgroundColor = #colorLiteral(red: 0.8832117915, green: 0.8782526851, blue: 0.8868549466, alpha: 1)
+                txtMesaj.textColor = .black
+            }
+            
         }
+    }
+    var mesajConstraint : AnchorConstraints!
+    override func viewleriOlustur() {
+        super.viewleriOlustur()
+        
+        addSubview(mesajContainer)
+        mesajContainer.layer.cornerRadius = 15
+         mesajConstraint =  mesajContainer.anchor(top: topAnchor, bottom: bottomAnchor, leading: leadingAnchor, trailing: trailingAnchor)
+        
+        mesajConstraint.leading?.constant = 20
+        mesajConstraint.trailing?.isActive = false
+        
+        mesajConstraint.trailing?.constant = -20
+        //Sağ Tarafta
+        
+        mesajContainer.widthAnchor.constraint(lessThanOrEqualToConstant: 260).isActive = true
+        //mesajContainer.genislikAyarla(260)
+        mesajContainer.addSubview(txtMesaj)
+        //txtMesaj.doldurSuperView()
+        txtMesaj.doldurSuperView(padding: .init(top: 5, left: 13, bottom: 5, right: 13))
+        
     }
     
 }
@@ -43,10 +92,10 @@ class MesajKayitController : ListeController<MesajCell,Mesaj>{
         navBar.btnGeri.addTarget(self, action: #selector(btnGeriPressed), for: .touchUpInside)
         
         veriler = [
-            .init(text: "Udemy Kursundan Herkese Selamlar"),
-            .init(text: "Udemy Kursundan Herkese Selamlar"),
-            .init(text: "Udemy Kursundan Herkese Selamlar"),
-            .init(text: "Udemy Kursundan Herkese Selamlar")
+            .init(text: "Udemy Kursundan Herkese Selamlar.Udemy Kursundan Herkese Selamlar.Udemy Kursundan Herkese Selamlar.Udemy Kursundan Herkese Selamlar.Udemy Kursundan Herkese Selamlar.Udemy Kursundan Herkese Selamlar.Udemy Kursundan Herkese Selamlar.Udemy Kursundan Herkese Selamlar.Udemy Kursundan Herkese Selamlar.Udemy Kursundan Herkese Selamlar", benimMesajim: false ),
+            .init(text: "Nasılsın", benimMesajim: true),
+            .init(text: "Udemy Kursundan Herkese Selamlar", benimMesajim: false),
+            .init(text: "Udemy Kursundan Herkese Selamlar", benimMesajim: true)
         ]
         
         view.addSubview(navBar)
@@ -54,6 +103,14 @@ class MesajKayitController : ListeController<MesajCell,Mesaj>{
         
         //collectionview hücrelerini belirtilen boşluktan itibaren oluşturmaya başlar.
         collectionView.contentInset.top = navBarYukseklik
+        
+        
+        let statusBar = UIView(arkaPlanRenk: .white)
+        view.addSubview(statusBar)
+        statusBar.anchor(top: view.topAnchor, bottom: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, trailing: view.trailingAnchor)
+        
+        collectionView.verticalScrollIndicatorInsets.top = navBarYukseklik
+        
     }
     
     
@@ -69,7 +126,18 @@ class MesajKayitController : ListeController<MesajCell,Mesaj>{
 extension MesajKayitController : UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return .init(width: view.frame.width, height: 110)
+        
+        
+        let tahminiHucre = MesajCell(frame: .init(x: 0, y: 0, width: view.frame.width, height: 1000))
+        
+        //mesaj içeriğini hücreye aktardık
+        tahminiHucre.veri = self.veriler[indexPath.item]
+        
+        tahminiHucre.layoutIfNeeded()
+        
+        let tahminiBoyut = tahminiHucre.systemLayoutSizeFitting(.init(width: view.frame.width, height: 1000))
+        
+        return .init(width: view.frame.width, height: tahminiBoyut.height)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
